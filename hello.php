d0b8edfb7f6a406a5e5d1890295c43ae2c5ecd2e
Pruek62 <6206021610145@fitm.kmutnb.ac.th>
2021-01-13 10:20:33 +0700

    update

---
+++ hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viwepoint" content="width=device-width,intial-scale=1.0"> 
+        <title>Document</title>
+        </head>
+        <body>
+            <?php>
+            echo "<h1>Hello</h1><br>";
+            echo "<h2>Prachiburi</h2>"
+            ?>
+            </body>
+            </html>
